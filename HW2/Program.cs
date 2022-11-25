7d834e66d3ae3cedc46d650fb30885e0fbb47cf9
AlexandrIstomin <108895194+AlexandrIstomin@users.noreply.github.com>
2022-11-25 18:42:01 +0300

    задача 2

---
+++ HW2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2: Напишите программу, 
+// которая принимает на вход три числа
+//  и выдаёт максимальное из этих чисел.
+
+Console.WriteLine();
+int a = 44; 
+int b = 5; 
+int c = 78;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine("max = ");
+Console.WriteLine(max);
