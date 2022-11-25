d0ef82dceadb00a6f86bc089cfd20deb07a2eb14
AlexandrIstomin <108895194+AlexandrIstomin@users.noreply.github.com>
2022-11-25 18:25:50 +0300

    первая задача

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 1:Напишите программу, 
+// которая на вход принимает два числа и выдаёт,
+//  какое число большее, а какое меньшее.
+
+Console.WriteLine();
+int numbera = 5; 
+int numberb = 7; 
+if (numbera > numberb)
+{
+    Console.WriteLine("max = " + numbera);
+    Console.WriteLine("min = " + numberb);
+}
+else
+{
+    Console.WriteLine("max = " + numberb);
+    Console.WriteLine("min = " + numbera);
+}
+
+
+
